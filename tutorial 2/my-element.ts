aac3b0a036a08a1c46c6a23d27b60ff9e7f567da
Yarely-Cutz-Ek <yarely.19070025@itsmotul.edu.mx>
2023-02-10 16:33:43 -0600

    tutorial2

--- tutorial 2/my-element.ts
+++ tutorial 2/my-element.ts
@@ -1,18 +1,46 @@
-paso 3
+paso 4
 import {LitElement, html} from 'lit';
 import {customElement, state} from 'lit/decorators.js';
+import type {TemplateResult} from 'lit';
 
 @customElement('my-element')
 class MyElement extends LitElement {
   @state()
-  names = ['Chandler', 'Phoebe', 'Joey', 'Monica', 'Rachel', 'Ross'];
+  friends = ['Harry', 'Ron', 'Hermione'];
+
+  @state()
+  pets = [
+    { name: "Hedwig", species: "Owl" },
+    { name: "Scabbers", species: "Rat" },
+    { name: "Crookshanks", species: "Cat" },
+  ];
+
+  @state()
+  includePets = true;
 
   render() {
+    const listItems: TemplateResult[] = [];
+    this.friends.forEach((friend) => {
+      listItems.push(html`<li>${friend}</li>`);
+    });
+    if (this.includePets) {
+      this.pets.forEach((pet) => {
+        listItems.push(html`<li>${pet.name} (${pet.species})</li>`);
+      });
+    }
+
     return html`
-      <p>A list of names that include the letter "e"</p>
+      <button @click=${() => this._togglePetVisibility()}>
+        ${this.includePets ? 'Hide' : 'Show'} pets
+      </button>
+      <p>My magical friends</p>
       <ul>
-        <!-- TODO: Render list items of filtered names. -->
+        ${listItems}
       </ul>
     `;
+  }
+
+  private _togglePetVisibility() {
+    this.includePets = !this.includePets;
   }
 }
