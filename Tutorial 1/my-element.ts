0f45d48cb0617234a801f2fc1de9cc398dac1b70
Yarely-Cutz-Ek <yarely.19070025@itsmotul.edu.mx>
2023-02-10 16:14:31 -0600

    tutorial1

--- Tutorial 1/my-element.ts
+++ Tutorial 1/my-element.ts
@@ -1,21 +1,22 @@
-paso 4
+paso 5
 import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
-@customElement('name-tag')
-export class NameTag extends LitElement {
+@customElement('more-expressions')
+export class MoreExpressions extends LitElement {
   @property()
-  name: string = 'Your name here';
+  checked: boolean = false;
 
   render() {
     return html`
-      <p>Hello, ${this.name}</p>
-      <input @input=${this.changeName} placeholder="Enter your name">
+      <div>
+        <input type="text" ?disabled=${!this.checked} value="Hello there.">
+      </div>
+      <label><input type="checkbox" @change=${this.setChecked}> Enable editing</label>
     `;
   }
 
-  changeName(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.name = input.value;
+  setChecked(event: Event) {
+    this.checked = (event.target as HTMLInputElement).checked;
   }
 }
