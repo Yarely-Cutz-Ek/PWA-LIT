1ab96eea29078a2f3d79feb5fdeddbd555625e25
Yarely-Cutz-Ek <yarely.19070025@itsmotul.edu.mx>
2023-02-12 17:04:14 -0600

    tutorial7

--- tutorial 7/word-viewer.ts
+++ tutorial 7/word-viewer.ts
@@ -1,14 +1,36 @@
-paso 5
-import { html, LitElement } from 'lit';
+paso 6
+import { css, html, LitElement } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 
 @customElement('word-viewer')
 class WordViewer extends LitElement {
+  static styles = css`
+    :host {
+      background-color: white;
+      color: violet;
+      cursor: pointer;
+      display: block;
+    }
+    pre {
+      padding: 0.2em;
+    }
+  `;
+
   @state() private idx = 0;
   @property() words = 'initial value';
 
-  // TODO: Define a `connectedCallback` which sets an interval.
-  // TODO: Define a `disconnectedCallback` which clears the interval.  
+  private intervalTimer?: number;
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.intervalTimer = setInterval(this.tickToNextWord, 1000);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    clearInterval(this.intervalTimer);
+    this.intervalTimer = undefined;
+  }
 
   render() {
     const splitWords = this.words.split('.');
@@ -16,5 +38,5 @@ class WordViewer extends LitElement {
     return html`<pre>${word}</pre>`;
   }
 
-  // TODO: Define `tickToNextWord`.
+  tickToNextWord = () => { this.idx += 1; };
 }
