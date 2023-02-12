c9bdb9ad032e91bbfd4293910a699800d8b000ea
Yarely-Cutz-Ek <yarely.19070025@itsmotul.edu.mx>
2023-02-12 17:01:16 -0600

    tutorial7

--- tutorial 7/word-viewer.ts
+++ tutorial 7/word-viewer.ts
@@ -1,12 +1,15 @@
-paso 3
+paso 4
 import { html, LitElement } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, property, state } from 'lit/decorators.js';
 
 @customElement('word-viewer')
 class WordViewer extends LitElement {
+  // TODO: Add `idx` state
   @property() words = 'initial value';
 
   render() {
-    return html`<pre>${this.words}</pre>`
+    // TODO: Split the `words` by `'.'`, and from the resulting word array
+    // only show the word on index `this.idx`.
+    return html`<pre>${this.words}</pre>`;
   }
 }
