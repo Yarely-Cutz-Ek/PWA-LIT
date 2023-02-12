b4c1aa8f3cdc762efa7b7a0c1bbc4ba0a2218d28
Yarely-Cutz-Ek <yarely.19070025@itsmotul.edu.mx>
2023-02-12 11:44:57 -0600

    tutorial6

--- tutorial 6/motion-carousel.ts
+++ tutorial 6/motion-carousel.ts
@@ -1,5 +1,5 @@
-Paso 3
-import {LitElement, html} from 'lit';
+Paso 4
+import {LitElement, html, PropertyValues} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import {styles} from './styles.js';
 
@@ -7,12 +7,40 @@ import {styles} from './styles.js';
 export class MotionCarousel extends LitElement {
   static styles = styles;
 
+  private selectedInternal = 0;
+  @property({type: Number})
+  selected = 0;
+
+  get maxSelected() {
+    return this.childElementCount - 1;
+  }
+
+  hasValidSelected() {
+    return this.selected >= 0 && this.selected <= this.maxSelected;
+  }
+
   render() {
+    if (this.hasValidSelected()) {
+      this.selectedInternal = this.selected;
+    }
     return html`
       <div class="fit">
-        <slot></slot>
+        <slot name="selected"></slot>
       </div>
     `;
+  }
+
+  private previous = 0;
+  protected updated(changedProperties: PropertyValues) {
+    if (changedProperties.has('selected') && this.hasValidSelected()) {
+      this.updateSlots();
+      this.previous = this.selected;
+    }
+  }
+
+  private updateSlots() {
+    this.children[this.previous]?.removeAttribute('slot');
+    this.children[this.selected]?.setAttribute('slot', 'selected');
   }
 
 }
